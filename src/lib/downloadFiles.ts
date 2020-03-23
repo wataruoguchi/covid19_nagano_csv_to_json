6321180fb2065fd05d9de2cb2e52fd389495b0a9
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2020-03-23 16:03:16 -0700

    Bugfix - promise.all

--- src/lib/downloadFiles.ts
+++ src/lib/downloadFiles.ts
@@ -4,15 +4,15 @@ import { item } from "./types";
 async function downloadFiles(urls: string[], dir: string): Promise<item[]> {
   // Download files (CSV file, probably made with MS Excel).
   const items: item[] = [];
-  await Promise.all(
-    urls.map(url => {
-      download(url, dir, { encoding: "binary" })
-        .then((data: string) => {
-          items.push({ path: url, data });
-        })
-        .catch(() => console.log(`LOG: ${url} got an error.`));
-    })
-  );
+  const promises = urls.map(async (url: string) => {
+    try {
+      const data: string = await download(url, dir, { encoding: "binary" });
+      items.push({ path: url, data });
+    } catch (err) {
+      console.log(`LOG: ${url} got an error.`);
+    }
+  });
+  await Promise.all(promises);
   return items;
 }
 
