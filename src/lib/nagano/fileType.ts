fb1acd97bc0ced6e3a3000d6172160ce44fe794c
kohei ikeda <tdn.suama@gmail.com>
2020-11-30 00:18:27 +0900

    taking care of suffix number

--- src/lib/nagano/fileType.ts
+++ src/lib/nagano/fileType.ts
@@ -3,12 +3,16 @@ import { CONST_PATIENTS, CONST_TEST_COUNT, CONST_CALL_CENTER } from "./const";
 
 function determineFileTypeByFileName(filePath: string): string {
   const fileName = getFileNameFromPath(filePath);
-  if (fileName === "200000_nagano_covid19_patients.csv") return CONST_PATIENTS;
-  if (fileName === "200000_nagano_covid19_test_count.csv")
+
+  if (fileName.startsWith("200000_nagano_covid19_patients"))
+    return CONST_PATIENTS;
+  if (fileName.startsWith("200000_nagano_covid19_test_count"))
     return CONST_TEST_COUNT;
-  if (fileName === "200000_nagano_covid19_call_center.csv")
+  if (fileName.startsWith("200000_nagano_covid19_call_center"))
     return CONST_CALL_CENTER;
+
   return CONST_CALL_CENTER;
 }
+
 
 export { determineFileTypeByFileName };
